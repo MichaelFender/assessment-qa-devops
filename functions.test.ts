d55a890021787fb5fc99f949ffa8712680bfbb9c
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-19 22:12:39 -0600

    added rollbar

---
+++ functions.test.ts
@@ -0,0 +1,7 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    test('shuffleArray should return an array', () => {
+        let formattedTitle = shuffleArray(testData..title)
+        expect(typeof formattedTitle).toBe('string')
+      })
